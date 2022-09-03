237e12ee7823c37d4afea437c50ca2a0bc239a9e
GKolesnikova <dvizenie77@gmail.com>
2022-09-03 12:16:02 +0200

    File rename

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php 
+namespace App\Controllers;
+
+class Main {
+	
+	public function defaultAction () 
+	{
+		$view = new \App\View();
+		echo $view->render('index.php');
+	}
+	
+	public function cityAction () 
+	{
+		$repo = new \App\Models\SightsRepo(
+			new \App\DB()
+		);
+
+		$view = new \App\View();
+		echo $view->render(
+			'about_my_city.php',
+			['sightsList' =>  $repo->getAllSights()]
+		);   
+		
+		
+		public function myAction () 
+	{
+		$view = new \App\View();
+		echo $view->render('about_me.php');
+	}
